927ec6d321a4a0d182b69246ac2fafe3e061021f
Victor <theonlypwner@users.noreply.github.com>
2011-01-23 16:55:41 +0000

    Changed position to a speedometer

--- source/src/renderhud.cpp
+++ source/src/renderhud.cpp
@@ -747,9 +747,13 @@ void gl_drawhud(int w, int h, int curfps, int nquads, int curvert, bool underwat
 	if(!hidestats)
 	{
 		const int left = (VIRTW-225-10)*2, top = (VIRTH*7/8)*2;
+		/*
 		draw_textf("x %05.3f", left, top-240, p->o.x);
 		draw_textf("y %05.3f", left, top-160, p->o.y);
 		draw_textf("z %04.1f", left, top-80, p->o.z);
+		*/
+		draw_textf("sp2 %05.3f", left, top-160, p->vel.magnitudexy());
+		draw_textf("spd %04.1f", left, top-80, p->vel.magnitude());
 		draw_textf("fps %d", left, top, curfps);
 		draw_textf("lod %d", left, top+80, lod_factor());
 		draw_textf("wqd %d", left, top+160, nquads);
