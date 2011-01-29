e192c433aa98b3444f42228aed6bcb6b12fb4380
Victor <theonlypwner@users.noreply.github.com>
2011-01-29 23:41:55 +0000

    Render nade icon fix

--- source/src/renderhud.cpp
+++ source/src/renderhud.cpp
@@ -294,8 +294,8 @@ void drawequipicons(playerent *p)
 	// weapons
 	int c = p->weaponsel->type, r = 0;
 	if(c == GUN_GRENADE){// draw nades separately
-		if(p->prevweaponsel->type != GUN_GRENADE) c = p->prevweaponsel->type;
-		else if(p->nextweaponsel->type != GUN_GRENADE) c = p->nextweaponsel->type;
+		if(p->prevweaponsel && p->prevweaponsel->type != GUN_GRENADE) c = p->prevweaponsel->type;
+		else if(p->nextweaponsel && p->nextweaponsel->type != GUN_GRENADE) c = p->nextweaponsel->type;
 		else c = 14; // unknown = HP symbol
 	}
 	else if(c == GUN_AKIMBO) c = GUN_PISTOL; // same icon for akimbo & pistol
@@ -904,8 +904,8 @@ void gl_drawhud(int w, int h, int curfps, int nquads, int curvert, bool underwat
 			// The next set will alter the matrix - load the identity matrix and apply ortho after
 			if(p->weaponsel && p->weaponsel->type>=GUN_KNIFE && p->weaponsel->type<NUMGUNS){
 				if(p->weaponsel->type != GUN_GRENADE) p->weaponsel->renderstats();
-				else if(p->prevweaponsel->type != GUN_GRENADE) p->prevweaponsel->renderstats();
-				else if(p->nextweaponsel->type != GUN_GRENADE) p->nextweaponsel->renderstats();
+				else if(p->prevweaponsel && p->prevweaponsel->type != GUN_GRENADE) p->prevweaponsel->renderstats();
+				else if(p->nextweaponsel && p->nextweaponsel->type != GUN_GRENADE) p->nextweaponsel->renderstats();
 			}
 			popfont();
 		}
