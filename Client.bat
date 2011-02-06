24573616d69846c7872cdb4b506c5444ee6d69b4
Victor <theonlypwner@users.noreply.github.com>
2011-02-06 21:04:12 +0000

    batch file improvement

--- Client.bat
+++ Client.bat
@@ -1,1 +1,1 @@
-bin_win32\ac_client.exe --home="home" --init %1 %2 %3 %4 %5
+bin_win32\ac_client.exe --home="home" --init %*
--- Server.bat
+++ Server.bat
@@ -1,1 +1,1 @@
-bin_win32\ac_server.exe -Cconfig/servercmdline.txt %1 %2 %3 %4 %5
+bin_win32\ac_server.exe -Cconfig/servercmdline.txt %*
