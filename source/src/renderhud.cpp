dd2adeab54e8ff1e05b9342a35a01ccade548678
Victor <theonlypwner@users.noreply.github.com>
2011-04-02 00:05:21 +0000

    Dropped flags radar fix

--- source/src/renderhud.cpp
+++ source/src/renderhud.cpp
@@ -518,15 +518,14 @@ void drawradar(playerent *p, int w, int h)
 		{
 			flaginfo &f = flaginfos[i];
 			entity *e = f.flagent;
-			if(!e) continue;
+			if(!e || e->x == -1 && e-> y == -1) continue;
 			float yaw = showmap ? 0 : camera1->yaw;
 			if(insideradar(centerpos, res/2, vec(e->x, e->y, centerpos.z)))
 				drawradarent(vec(e->x, e->y, 0), coordtrans, yaw, m_ktf && f.state!=CTFF_IDLE ? 2 : f.team, 3, iconsize, false); // draw bases
 			if(m_ktf && f.state==CTFF_IDLE) continue;
 			vec pos(0.5f-0.1f, 0.5f-0.9f, 0);
 			pos.mul(iconsize/coordtrans).rotate_around_z(yaw*RAD);
-			if(f.state==CTFF_STOLEN)
-			{
+			if(f.state==CTFF_STOLEN){
 				if(f.actor)
 				{
 					pos.add(f.actor->o);
@@ -535,12 +534,16 @@ void drawradar(playerent *p, int w, int h)
 						drawradarent(pos, coordtrans, yaw, 3, m_ktf ? 2 : f.team, iconsize, true); // draw near flag thief
 				}
 			}
-			else
-			{
+			else{
+				/*
 				pos.x += e->x;
 				pos.y += e->y;
 				pos.z += centerpos.z;
-				if(insideradar(centerpos, res/2, pos)) drawradarent(pos, coordtrans, yaw, 3, m_ktf ? 2 : f.team, iconsize, false); // draw on entitiy pos
+				*/
+				pos.x += f.pos.x;
+				pos.y += f.pos.y;
+				pos.z += f.pos.z;
+				if(insideradar(centerpos, res/2, pos)) drawradarent(pos, coordtrans, yaw, 3, m_ktf ? 2 : f.team, iconsize, false);
 			}
 		}
 	}
