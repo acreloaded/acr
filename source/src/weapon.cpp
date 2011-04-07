e8c6f5c30aefdb06c715e9d39624cf52328749bf
Victor <theonlypwner@users.noreply.github.com>
2011-04-07 01:59:27 +0000

    More spread

--- source/src/weapon.cpp
+++ source/src/weapon.cpp
@@ -639,7 +639,7 @@ void weapon::renderhudmodel() { renderhudmodel(owner->lastaction); }
 void weapon::renderaimhelp(int teamtype) { if(owner->ads != 1000) drawcrosshair(owner, CROSSHAIR_DEFAULT, teamtype); }
 int weapon::dynspread() {
 	if(info.spread <= 1) return 1;
-	return (int)(info.spread * (owner->vel.magnitude() / 3.f + owner->pitchvel / 5.f + 0.4f) * 1.4f * owner->eyeheight / owner->maxeyeheight * (1 - owner->ads / 1000.f));
+	return (int)(info.spread * (owner->vel.magnitude() / 3.f + owner->pitchvel / 5.f + 0.4f) * 2.4f * owner->eyeheight / owner->maxeyeheight * (1 - owner->ads / 1000.f));
 }
 float weapon::dynrecoil() { return info.kick * (1 - owner->ads / 2000.f); }
 bool weapon::selectable() { return this != owner->weaponsel && owner->state == CS_ALIVE && !owner->weaponchanging; }
