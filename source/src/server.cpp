29fea693d7b54fca3e6b3bd29a9fe326e66ee8ad
Victor <theonlypwner@users.noreply.github.com>
2011-04-22 17:26:46 +0000

    Actor doesn't have to be alive in order to damage

--- source/src/server.cpp
+++ source/src/server.cpp
@@ -1295,7 +1295,7 @@ void forcedeath(client *cl, bool gib = false){
 }
 
 void serverdamage(client *target, client *actor, int damage, int gun, bool gib, const vec &source){
-	if(!target || !actor || target->state.state != CS_ALIVE || actor->state.state != CS_ALIVE) return;
+	if(!target || !actor || target->state.state != CS_ALIVE) return;
 	if(m_expert && !gib && gun != GUN_KNIFE) return;
 	clientstate &ts = target->state;
 	if(target!=actor){
