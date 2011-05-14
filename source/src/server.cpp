a890fc3d7a1eb99562436803d6bdd72ebf5cdf9f
Victor <theonlypwner@users.noreply.github.com>
2011-05-14 22:25:17 +0000

    Simplify in map checking

--- source/src/server.cpp
+++ source/src/server.cpp
@@ -2667,6 +2667,37 @@ void sendwelcome(client *cl, int chan, bool forcedeath){
 	cl->haswelcome = true;
 }
 
+bool fixsposinmap(const vec &p, vec *fixed = NULL){
+	bool ret = false;
+	vec fix = p;
+	if(maplayout){
+		// xy
+		loopi(2){
+			if(fix[i] < 2){
+				fix[i] = 2;
+				ret = true;
+			}
+			else if(fix[i] > (1 << maplayout_factor) - 2){
+				fix[i] = (1 << maplayout_factor) - 2;
+				ret = true;
+			}
+		}
+		if(!ret){
+		// z
+		const char ceil = 128, floor = maplayout[((int)fix.x) + (((int)fix.y) << maplayout_factor)];
+		if(fix.z > ceil){
+			fix.z = ceil;
+			ret = true;
+		}
+		else if(floor > fix.z + 3)
+			fix.z = floor;
+			ret = true;
+		}
+	}
+	if(fixed) *fixed = fix;
+	return ret;
+}
+
 void checkmove(client &cp){
 	const int sender = cp.clientnum;
 	clientstate &cs = cp.state;
