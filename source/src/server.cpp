bdd8490a78a5580d1254f1ba8f6b8be17e672d92
Victor <theonlypwner@users.noreply.github.com>
2011-05-16 00:09:40 +0000

    Working server newmap (enlarge map needs fixes)

--- source/src/server.cpp
+++ source/src/server.cpp
@@ -3509,18 +3509,24 @@ void process(ENetPacket *packet, int sender, int chan)   // sender may be -1
 
 			case N_NEWMAP: // the server needs to create a new layout
 			{
-				maplayout_factor = getint(p);
+				const int size = getint(p);
+				if(size < 0) maplayout_factor++;
+				else maplayout_factor = size;
 				QUEUE_MSG;
-				const int layoutsize = 1 << (maplayout_factor * 2);
-				DELETEA(mapfloor);
-				DELETEA(mapfhfbase);
-				DELETEA(mapceil);
-				mapfloor = new char[layoutsize + 256];
-				mapfhfbase = new char[layoutsize + 256];
-				mapceil = new char[layoutsize + 256];
-				memset(mapfloor, 0, layoutsize * sizeof(char));
-				memset(mapfhfbase, 0, layoutsize * sizeof(char));
-				memset(mapceil, 16, layoutsize * sizeof(char));
+				if(maplayout_factor >= 0){
+					maplayout_factor = clamp(maplayout_factor, SMALLEST_FACTOR, LARGEST_FACTOR);
+					swaterlvl = -100000;
+					const int layoutsize = 1 << (maplayout_factor * 2);
+					DELETEA(mapfloor);
+					DELETEA(mapfhfbase);
+					DELETEA(mapceil);
+					mapfloor = new char[layoutsize + 256];
+					mapfhfbase = new char[layoutsize + 256];
+					mapceil = new char[layoutsize + 256];
+					memset(mapfloor, 0, layoutsize * sizeof(char));
+					memset(mapfhfbase, 0, layoutsize * sizeof(char));
+					memset(mapceil, 26, layoutsize * sizeof(char));
+				}
 				break;
 			}
 
