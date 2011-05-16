bf7bda5dfcf08038f0e52f36b6834f4f2aa2cb25
Victor <theonlypwner@users.noreply.github.com>
2011-05-16 01:19:00 +0000

    Translucent entities after picking them up

--- source/src/entities.cpp
+++ source/src/entities.cpp
@@ -25,7 +25,7 @@ void renderent(entity &e)
 	const char *mdlname = entmdlnames[e.type-I_CLIPS];
 	float z = (float)(1+sinf(lastmillis/100.0f+e.x+e.y)/20),
 		  yaw = lastmillis/10.0f;
-	rendermodel(mdlname, ANIM_MAPMODEL|ANIM_LOOP|ANIM_DYNALLOC, 0, 0, vec(e.x, e.y, z+S(e.x, e.y)->floor+e.attr1), yaw, 0);
+	rendermodel(mdlname, ANIM_MAPMODEL|(e.spawned ? 0 : ANIM_TRANSLUCENT)|ANIM_LOOP|ANIM_DYNALLOC, 0, 0, vec(e.x, e.y, z+S(e.x, e.y)->floor+e.attr1), yaw, 0);
 }
 
 void renderclip(entity &e)
@@ -101,7 +101,7 @@ void renderentities()
 		entity &e = ents[i];
 		if(isitem(e.type))
 		{
-			if((!OUTBORD(e.x, e.y) && e.spawned) || editmode)
+			if(!OUTBORD(e.x, e.y) || editmode)
 			{
 				renderent(e);
 			}
