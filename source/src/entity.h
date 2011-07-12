39f0d8b8d23489ca9ba6570ddfda1f7df107244c
Victor <theonlypwner@users.noreply.github.com>
2011-07-12 23:27:34 +0000

    Changing more stats of the crossbow

--- source/src/entity.h
+++ source/src/entity.h
@@ -115,7 +115,7 @@ static guninfo guns[NUMGUNS] =
 	{ "pistol",     S_PISTOL,   S_RAKIMBO,  1400,   80,     40,   45,  160,   20,   0,   0, 56,    8,   24,   6,  2,    28,    48,     70, 2,   true },
 	{ "knife",      S_KNIFE,    S_NULL,        0,   500,    80,    3,    4,   60,   0,   0,  1,    1,   1,    0,  0,     0,    0,       0, 5,   true },
 	{ "wave",       S_NULL,     S_RASSAULT, 2500,   40,     10,    8,   12,    6,   0,   0,168,    2,   50,   0,  2,    16,   19,     360, 0,   true },
-	{ "bow",        S_NULL,     S_RASSAULT, 2500,   2000,  250,    0,   24,  250,   0,   0, 48,    1,    1,   3,  1,    48,   64,       0, 4,   true },
+	{ "bow",        S_NULL,     S_RASSAULT, 2000,   120,   250,    0,   24,  250,   0,   0, 48,    3,    1,   3,  1,    48,   64,       0, 4,   true },
 };
 
 static inline ushort reloadtime(int gun) { return guns[gun].reloadtime; }
