34a25d57101eb7362f4291369e1a094403b19138
Victor <theonlypwner@users.noreply.github.com>
2011-07-13 04:46:02 +0000

    Yellow tint for the crossbow explosion

--- source/src/renderparticles.cpp
+++ source/src/renderparticles.cpp
@@ -264,7 +264,7 @@ static struct parttype { int type; float r, g, b; int gr, tex; float sz; } partt
 	{ PT_HUDFLASH,   1.0f, 1.0f, 1.0f, 0,  6, 0.7f  }, // hudgun muzzle flash 
 	{ PT_FLASH,	  1.0f, 1.0f, 1.0f, 0,  6, 0.7f  }, // muzzle flash 
 	{ PT_PART,	   1.0f, 0.5f, 0.2f, 20, 0, 0.08f }, // orange: edit mode closest ent
-	{ PT_FIREBALL,   1.0f, 0.5f, 0.5f, 0,  2, 3.0f  }, // crossbow explosion fireball
+	{ PT_FIREBALL,   1.0f, 1.0f, 0.5f, 0,  2, 3.0f  }, // crossbow explosion fireball
 };
 
 VAR(particlesize, 20, 100, 500);
