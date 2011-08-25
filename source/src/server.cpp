d000cc6c3dc8b4c67eb9b221a84a53295f2057a1
Victor <theonlypwner@users.noreply.github.com>
2011-08-25 17:34:31 +0000

    PERK_FALL -> PERK_LIGHT (higher jumps) Also, fix for not jumping as high with heavy weapons

--- source/src/server.cpp
+++ source/src/server.cpp
@@ -3803,7 +3803,7 @@ void process(ENetPacket *packet, int sender, int chan)   // sender may be -1
 				if(isfall){
 					if(cp->state.state != CS_ALIVE) break;
 					// deal falling damage?
-					int damage = (fall - 10) / (cp->state.perk == PERK_FALL ? 10 : 2);
+					int damage = (fall - 10) / (cp->state.perk == PERK_LIGHT ? 10 : 2);
 					if(damage < 1) break; // don't heal the player
 					else if(damage > 200) damage = 200;
 					serverdamage(cp, cp, damage, WEAP_MAX+2, FRAG_NONE, cp->state.o);
