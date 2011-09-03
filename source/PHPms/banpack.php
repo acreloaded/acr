3a80ad12333ea7ecadf5963bef7d32396ddb7410
Victor <theonlypwner@users.noreply.github.com>
2011-09-03 01:12:54 +0000

    banpack parser fix

--- source/PHPms/banpack.php
+++ source/PHPms/banpack.php
@@ -3,7 +3,7 @@
 	$banpack = array();
 	if(file_exists("banpack.txt") && ($f = @fopen("banpack.txt", "r"))) while (($buf = fgets($f, 81)) !== false ){
 		if($buf[0] == "/") continue; // speed it up!
-		if(!preg_match("#([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})(/[0-9]{1,2})?#s", $buf, $match)) continue;
+		if(!preg_match("#([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})(?:/([0-9]{1,2}))?#s", $buf, $match)) continue;
 		$start = $end = ip2long($match[1]);
 		if($match[2] >= 0 && $match[2] < 32){
 			$mask = (1 << $match[2]) - 1;
