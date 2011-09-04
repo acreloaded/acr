55d51edacf7822345a56849460ea8232c46dbb4d
Victor <theonlypwner@users.noreply.github.com>
2011-09-04 19:37:48 +0000

    banpack optimization

--- source/PHPms/auth.php
+++ source/PHPms/auth.php
@@ -3,5 +3,12 @@
 	$config['auth'] = array(
 		// NOTICE: use authgen.php to compute the imitation public-key of a private-key
 		// array('public', 'name', priv) // 1: master, 2: admin, 3: highest/owner
+		array('c12642f4ec193d9ddb39659e4600c89a7e40f544', 'Victor', 1),
+		array('722d3312e214d83cfb5c6d3a4835dc14ed18a1ce', 'Victor', 2),
+		array('15c5be5ae22490c3c96345d75833c26f494ce2ff', 'Victor', 3),
+		array('0e5e770591b35261f5d4d3782b8ac0e4a51519b5', 'Josh', 1),
+		array('79d5a286f56d0c29fed352553a0f2e72d70fe128', 'D4RKN3T', 1),
+		array('4d825129c9496546df2e37057cc924763c17556d', 'iLiamize', 1),
+		array('01d32d53884a90fa9b18bbba0c92ddfd553c98a6', 'glock', 1),
 	);
 ?>
--- source/PHPms/banpack.php
+++ source/PHPms/banpack.php
@@ -1,16 +1,5 @@
 <?
-	// server bans
-	$banpack = array();
-	if(file_exists("banpack.txt") && ($f = @fopen("banpack.txt", "r"))) while (($buf = fgets($f, 81)) !== false ){
-		if($buf[0] == "/") continue; // speed it up!
-		if(!preg_match("#([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})(?:/([0-9]{1,2}))?#", $buf, $match)) continue;
-		$start = $end = ip2long($match[1]);
-		if($match[2] > 0 && $match[2] < 32){
-			$mask = (1 << (32 - $match[2])) - 1;
-			$start &= ~$mask;
-			$end |= $mask;
-		}
-		$banpack[$start] = $end;
-	}
+	// server ban-pack
+	require "banpack_read.php";
 	foreach($banpack as $bs => $be) $config['sbans'] []= array($bs, $be, 1);
 ?>
