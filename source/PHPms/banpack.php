93af63600fc7abc384dc63ea13dfddb407f4dd45
Victor <theonlypwner@users.noreply.github.com>
2011-09-04 20:33:08 +0000

    banpack should block registration too

--- source/PHPms/banpack.php
+++ source/PHPms/banpack.php
@@ -1,5 +1,5 @@
 <?
 	// server ban-pack
 	require "banpack_read.php";
-	foreach($banpack as $bs => $be) $config['sbans'] []= array($bs, $be, 1);
+	foreach($banpack as $bs => $be) $config['sbans'] []= array($bs, $be, 3); // 1: play, 2: register, 3: all
 ?>
