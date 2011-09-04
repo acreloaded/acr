83306dd54c31d4a2e8445a33d4f04fa7d6e31813
Victor <theonlypwner@users.noreply.github.com>
2011-09-04 19:09:54 +0000

    mistakes were made with the banning...

--- source/PHPms/banpack.php
+++ source/PHPms/banpack.php
@@ -5,8 +5,8 @@
 		if($buf[0] == "/") continue; // speed it up!
 		if(!preg_match("#([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})(?:/([0-9]{1,2}))?#", $buf, $match)) continue;
 		$start = $end = ip2long($match[1]);
-		if($match[2] >= 0 && $match[2] < 32){
-			$mask = (1 << $match[2]) - 1;
+		if($match[2] > 0 && $match[2] < 32){
+			$mask = (1 << (32 - $match[2])) - 1;
 			$start &= ~$mask;
 			$end |= $mask;
 		}
