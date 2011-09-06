d2838e61deaf9a38ff5c2a61c94fb4b84a4e5cb4
Victor <theonlypwner@users.noreply.github.com>
2011-09-06 22:50:10 +0000

    better ms ban list

--- source/PHPms/index.php
+++ source/PHPms/index.php
@@ -60,13 +60,13 @@
 		$banned = $allowed = 0;
 		foreach($config['sbans'] as $r){
 			if($r[0] <= $ip && $ip <= $r[1]){ ++$banned; echo "*"; }
-			echo "ban ".long2ip($r[0]).($r[0] != $r[1] ? " - ".long2ip($r[1]) : '')." from ".banflag2s($r[2]).($r[3] ? " because ".$r[3] : "")."<br>\n";
+			echo "ban ".long2ip($r[0]).($r[0] != $r[1] ? " - ".long2ip($r[1]) : '')." from ".banflag2s($r[2]).($r[3] ? "; remarks: ".$r[3] : "")."<br>\n";
 		}
 		foreach($config['sallows'] as $r){
 			if($r[0] <= $ip && $ip <= $r[1]){ ++$allowed; echo "*"; }
-			echo "allow ".long2ip($r[0]).($r[0] != $r[1] ? " - ".long2ip($r[1]) : '')." to be ".banflag2s($r[2]).($r[3] ? " because ".$r[3] : "")."<br>\n";
+			echo "allow ".long2ip($r[0]).($r[0] != $r[1] ? " - ".long2ip($r[1]) : '')." to be ".banflag2s($r[2]).($r[3] ? "; remarks: ".$r[3] : "")."<br>\n";
 		}
-		echo "Your IP is: ".long2ip($ip)." which is banned {$banned} times and allowed {$allowed} times<br>\nthat is all.";
+		echo "Your IP is: ".long2ip($ip)." which is banned {$banned} times and allowed {$allowed} times<br>\nThat is all.";
 	}
 	elseif(isset($_GET['register'])){ // register
 		function addserver($ip, $port, $add){ // returns if it is renewed
