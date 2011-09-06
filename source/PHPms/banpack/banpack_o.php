4432aa35166b269610beaf9b4a3b5600a11ebc6a
Victor <theonlypwner@users.noreply.github.com>
2011-09-06 22:48:35 +0000

    better banpack optimizer notices

--- source/PHPms/banpack/banpack_o.php
+++ source/PHPms/banpack/banpack_o.php
@@ -5,12 +5,12 @@
 	foreach($banpack as $bs => $be) $banpack2[] = array($bs, $be);
 	for($i = 1; $i < sizeof($banpack2); ++$i){
 		if($banpack2[$i][1] <= $banpack2[$i - 1][1]){
-			echo "already covered";
+			echo "already covered ".long2ip($banpack2[$i][0])." - ".long2ip($banpack2[$i][1]);
 			unset($banpack2[$i--]);
 			$banpack2 = array_values($banpack2);
 		}
 		elseif($banpack2[$i][0] <= $banpack2[$i - 1][1] + 1){
-			echo "overlap ".long2ip($banpack2[$i][0])." - ".long2ip($banpack2[$i][1])." with ".long2ip($banpack2[$i - 1][0])." - ".long2ip($banpack2[$i - 1][1])." \n";
+			echo "overlap ".long2ip($banpack2[$i - 1][0])." - ".long2ip($banpack2[$i - 1][1])." with ".long2ip($banpack2[$i][0])." - ".long2ip($banpack2[$i][1])." \n";
 			$banpack2[$i - 1][1] = $banpack2[$i][1];
 			unset($banpack2[$i--]);
 			$banpack2 = array_values($banpack2);
