16a7ee78c44907536303f830b75c3185b3d3eba8
Victor <theonlypwner@users.noreply.github.com>
2011-09-09 02:08:18 +0000

    don't replace client number with owner number in server logs

--- source/src/server.cpp
+++ source/src/server.cpp
@@ -4036,7 +4036,7 @@ void loggamestatus(const char *reason){
 	{
 		client &c = *clients[i];
 		if(c.type == ST_EMPTY || !c.name[0]) continue;
-		formatstring(text)("%2d%c %-16s ", c.state.ownernum < 0 ? c.clientnum : c.state.ownernum, c.state.ownernum < 0 ? ' ' : '*', c.name); // cn* name
+		formatstring(text)("%2d%c %-16s ", c.clientnum, c.state.ownernum < 0 ? ' ' : '*', c.name); // cn* name
 		if(m_team) concatformatstring(text, "%-4s ", team_string(c.team)); // team
 		if(m_flags) concatformatstring(text, "%4d ", c.state.flagscore);	 // flag
 		concatformatstring(text, "%4d %5d", c.state.frags, c.state.deaths);  // frag death
