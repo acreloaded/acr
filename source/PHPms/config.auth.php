d0d7e7a2268008df1118e8491631fef934bdcfa3
Victor <theonlypwner@users.noreply.github.com>
2011-09-12 00:35:57 +0000

    remove the useless authgen

--- source/PHPms/config.auth.php
+++ source/PHPms/config.auth.php
@@ -1,7 +1,7 @@
 <?
 	// authority authentication
 	$config['auth'] = array(
-		// NOTICE: use authgen.php to compute the imitation public-key of a private-key
-		// array('public', 'name', priv) // 1: master, 2: admin, 3: highest/owner
+		// uses a PSK (pre-shared key)
+		// array('key', 'name', priv) // 1: master, 2: admin, 3: highest/owner
 	);
 ?>
