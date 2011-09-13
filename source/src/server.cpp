cfd5db2284fefb6803214ba8eee3e486b9e3639f
Victor <theonlypwner@users.noreply.github.com>
2011-09-13 02:39:03 +0000

    fix master bans (again!)

--- source/src/server.cpp
+++ source/src/server.cpp
@@ -1771,7 +1771,7 @@ void addmrange(char *text){
 		target.add(ir);
 		ptr = strchr(end, '|');
 	}
-	target.sort(cmpipmatch); // or else bsearch fucks up
+	target.sort(cmpiprange); // or else bsearch fucks up
 }
 
 inline bool checkblacklist(enet_uint32 ip, vector<iprange> &ranges){ // ip: network byte order
