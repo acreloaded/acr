826c241246fb648af41b806546af5b75f7914249
Victor <theonlypwner@users.noreply.github.com>
2011-10-24 22:46:27 +0000

    moving the PHP master server to a separate project

--- source/PHPms/config.auth.php
+++
@@ -1,7 +0,0 @@
-<?
-	// authority authentication
-	$config['auth'] = array(
-		// uses a PSK (pre-shared key)
-		// array('key', 'name', priv) // 0:deban 1: master, 2: admin, 3: highest/owner
-	);
-?>
