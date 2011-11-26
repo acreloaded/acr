c8d672ee4e08cda348d4101cb5f7fa7126812ed1
Victor <theonlypwner@users.noreply.github.com>
2011-11-26 17:43:07 +0000

    12 + 3n auto zombies

--- source/src/aiman.h
+++ source/src/aiman.h
@@ -103,7 +103,7 @@ void checkai(){
 	if(!botbalance) balance = 0;
 	else if(people) switch(botbalance){
 		case -1: // auto
-			if(m_zombies) balance = 10 + 2 * people;
+			if(m_zombies) balance = 12 + 3 * people; // effectively 12 + 2n
 			else if(m_duel) balance = max(people, maplayout_factor - 3); // 3 - 5 - 8 (6 - 8 - 11 layout factor)
 			else{
 				const int spawns = m_team ? (smapstats.hasteamspawns ? smapstats.spawns[0] + smapstats.spawns[1] : 16) : (smapstats.hasffaspawns ? smapstats.spawns[2] : 6);
