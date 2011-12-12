c95bf392ce379f5325fb493661ad9224c3f17b42
Victor <theonlypwner@users.noreply.github.com>
2011-12-12 00:47:25 +0000

    fix for the double round start

--- source/src/server.cpp
+++ source/src/server.cpp
@@ -2209,7 +2209,7 @@ void resetmap(const char *newname, int newmode, int newtime, bool notify){
 	logline(ACLOG_INFO, "");
 	logline(ACLOG_INFO, "Game start: %s on %s, %d players, %d minutes remaining, mastermode %d, (%s'getmap' %sprepared)",
 		modestr(smode), smapname, numclients(), minremain, mastermode, ms ? "" : "itemlist failed,", mapavailable(smapname) ? "" : "not ");
-	arenaround = 0;
+	//arenaround = 0;
 	arenastart = gamemillis;
 	nokills = true;
 	if(m_duel) distributespawns();
