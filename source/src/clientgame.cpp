0d14165d2b40f870315567d4a82c9b2009c1ebde
Victor <theonlypwner@users.noreply.github.com>
2012-01-05 21:46:22 +0000

    no deathcam if you switch to spectator

--- source/src/clientgame.cpp
+++ source/src/clientgame.cpp
@@ -190,7 +190,7 @@ void deathstate(playerent *pl)
 {
 	if(pl == player1 && editmode) toggleedit(true);
 	pl->state = CS_DEAD;
-	pl->spectatemode = SM_DEATHCAM;
+	pl->spectatemode = pl->team == TEAM_SPECT ? SM_FLY : SM_DEATHCAM;
 	pl->respawnoffset = pl->lastpain = lastmillis;
 	pl->move = pl->strafe = pl->pitchvel = pl->pitchreturn = pl->ads = 0;
 	// position camera (used to be roll/pitch)
