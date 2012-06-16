c215e625ec3f0ea27109cb3dbccd50bfa123afe0
Victor <theonlypwner@users.noreply.github.com>
2012-06-16 20:11:26 +0000

    This might fix the damage screen bug

--- source/src/renderhud.cpp
+++ source/src/renderhud.cpp
@@ -709,7 +709,7 @@ void gl_drawhud(int w, int h, int curfps, int nquads, int curvert, bool underwat
 		const int maxhealth = 100 * HEALTHSCALE;
 		float newfade = (p->state != CS_DEAD && p->health >= 0 && p->state != CS_EDITING) ? ((1 - powf(clamp<float>(p->health, 0.f, maxhealth) / maxhealth, 2)) * damagescreenalpha / 100.f) : 0;
 		fade = clamp((fade * 40 + newfade) / 41.f, 0.f, 1.f);
-		if(fade){
+		if(fade >= 0.05f){
 			glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
 			glBindTexture(GL_TEXTURE_2D, damagetex->id);
 			glColor4f(fade, fade, fade, fade);
