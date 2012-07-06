d9e722162beef904aa619c5b9d1ae48998c08cc5
Victor <theonlypwner@users.noreply.github.com>
2012-07-06 21:05:39 +0000

    Fix confirm by subtracting the player height

--- source/src/entities.cpp
+++ source/src/entities.cpp
@@ -128,7 +128,7 @@ void renderentities()
 		const int fixteam = player1->team == TEAM_SPECT ? TEAM_BLUE : player1->team;
 		const float yaw = lastmillis/10.0f;
 		vec o = confirms[i].o;
-		o.z += (float)(1+sinf(lastmillis/100.0f+confirms[i].o.x+confirms[i].o.y)/20);
+		o.z += (float)(1+sinf(lastmillis/100.0f+confirms[i].o.x+confirms[i].o.y)/20) - PLAYERHEIGHT;
 		rendermodel(fixteam == confirms[i].team ? "pickups/flags/ktf" : "pickups/flags/RED_htf", ANIM_FLAG|ANIM_LOOP|ANIM_DYNALLOC, 0, 0, o, yaw, 0);
 	}
 	if(m_affinity(gamemode)) loopi(2)
