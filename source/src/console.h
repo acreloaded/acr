4a4cbfd1b28557dd82dc42bc6916e26900d27845
Victor <theonlypwner@users.noreply.github.com>
2012-07-22 18:13:48 +0000

    Fix killfeed condenser

--- source/src/console.h
+++ source/src/console.h
@@ -21,21 +21,21 @@ template<class LINE> struct consolebuffer
 
 	void addobit(const char *sf, int cn) {
 		extern int totalmillis;
-		if(conlines[0].obit == cn){ // last one was our kill
-			LINE &l = conlines[1];
-			if(l.obit == -1){ // not 2+ kills yet
+		if(conlines[0].obit == cn && conlines.length() > 2){ // last one was our kill
+			if(conlines[1].obit == -1){ // not 2+ kills yet
 				playerent *d = getclient(cn);
-				// THIS SHOULD NOT BE OVERWRITTEN?!
-				formatstring(l.line)("\f2+1 kill that was made by %s", d ? colorname(d) : "someone");
-				l.obit = 1;
+				formatstring(conlines[0].line)("\f2+1 kill that was made by %s", d ? colorname(d) : "someone");
+				conlines[0].obit = 1;
+				addline(sf, totalmillis, cn);
 			}
 			else{
 				playerent *d = getclient(cn);
-				formatstring(l.line)("\f1%+d kills that were made by %s", ++l.obit, d ? colorname(d) : "someone");
+				formatstring(conlines[1].line)("\f1%+d kills that were made by %s", ++conlines[1].obit, d ? colorname(d) : "someone");
+				conlines[1].millis = totalmillis;
+				// overwrite
+				copystring(conlines[0].line, sf);
+				conlines[0].millis = totalmillis;
 			}
-			l.millis = totalmillis;
-			copystring(conlines[0].line, sf);
-			conlines[0].millis = totalmillis;
 		}
 		else{
 			conlines[0].obit = -1; // force no overwrite
