e263abc4eca950a7af47df0f98260cfb4e73207c
Victor <theonlypwner@users.noreply.github.com>
2012-08-12 16:26:18 +0000

    more aggressive knifing from bots

--- source/src/bot/ac_bot_ai.cpp
+++ source/src/bot/ac_bot_ai.cpp
@@ -16,7 +16,7 @@
 // UNDONE
 weaponinfo_s WeaponInfoTable[MAX_WEAPONS] =
 {
-	{ TYPE_MELEE, 0.0f, 2.0f, 0.0f, 3.5f, 1 }, // KNIFE
+	{ TYPE_MELEE, 0.0f, 4.0f, 0.0f, 5.0f, 1 }, // KNIFE
 	{ TYPE_NORMAL, 0.0f, 20.0f, 0.0f, 50.0f, 6 }, // PISTOL
 	{ TYPE_SHOTGUN, 0.0f, 15.0f, 0.0f, 40.0f, 3 }, // SHOTGUN
 	{ TYPE_AUTO, 0.0f, 25.0f, 0.0f, 60.0f, 10 }, // SUBGUN
@@ -26,7 +26,7 @@ weaponinfo_s WeaponInfoTable[MAX_WEAPONS] =
 	{ TYPE_GRENADE, 30.0f, 25.0f, 0.0f, 50.0f, 1 }, // GRENADE
 	{ TYPE_NORMAL, 0.0f, 20.0f, 0.0f, 50.0f, 6 }, // AKIMBO?
 	{ TYPE_AUTO, 40.0f, 80.0f, 0.0f, 150.0f, 6 }, // HEAL
-	{ TYPE_MELEE, 0.0f, 4.0f, 0.0f, 7.0f, 1 }, // SWORD
+	{ TYPE_MELEE, 0.0f, 7.0f, 0.0f, 9.0f, 1 }, // SWORD
 	{ TYPE_ROCKET, 0.0f, 20.0f, 0.0f, 50.0f, 2 }, // RPG
 };
 
