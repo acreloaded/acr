08fa562542c872786ebd2e0a227bb74ab2720f76
Victor <theonlypwner@users.noreply.github.com>
2012-08-26 21:13:37 +0000

    Back to 80%

--- source/src/weapon.cpp
+++ source/src/weapon.cpp
@@ -437,7 +437,7 @@ void weapon::attackphysics(const vec &from, const vec &to) // physical fx to the
 	// kickback
 	owner->vel.add(vec(unitv).mul(kick * owner->eyeheight / owner->maxeyeheight));
 	// recoil
-	const float recoilshift = (rnd(info.recoilangle * 20 + 1) / 10.f - info.recoilangle) * RAD, recoilval = info.recoil * sqrtf(rnd(50) + 51) / (owner->perk1 == PERK1_HAND ? 100.f : 150.f);
+	const float recoilshift = (rnd(info.recoilangle * 20 + 1) / 10.f - info.recoilangle) * RAD, recoilval = info.recoil * sqrtf(rnd(50) + 51) / (owner->perk1 == PERK1_HAND ? 125.f : 100.f);
 	owner->pitchvel += cosf(recoilshift) * recoilval;
 	owner->yawvel += sinf(recoilshift) * recoilval;
 	const float maxmagnitude = sqrtf(owner->pitchvel * owner->pitchvel + owner->yawvel + owner->yawvel) / info.maxrecoil * 10;
