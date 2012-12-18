3a414ad7169cba77b3d55fcc07aa11a3611071cd
Victor <theonlypwner@users.noreply.github.com>
2012-12-18 23:23:16 +0000

    Disable menu close on new round.

--- source/src/clients2c.cpp
+++ source/src/clients2c.cpp
@@ -537,7 +537,7 @@ void parsemessages(int cn, playerent *d, ucharbuf &p)
 					setscope(false);
 					if(m_duke(gamemode, mutators)){
 						if(!m_zombie(gamemode) && !m_convert(gamemode, mutators)) arenaintermission = 0;
-						closemenu(NULL);
+						//closemenu(NULL);
 						conoutf("%s", _("spawn_newround"));
 						hudeditf(HUDMSG_TIMER, _("spawn_fight"));
 					}
