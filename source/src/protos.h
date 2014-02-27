f4a5eb0f95f789e3e595c66b539c4f1a68c1042f
theonlypwner <theonlypwner@users.noreply.github.com>
2014-02-26 21:20:03 -0700

    Fix by defining the inline function in the header

--- source/src/protos.h
+++ source/src/protos.h
@@ -873,12 +873,16 @@ extern void sendstring(const char *t, packetbuf &p);
 extern void sendstring(const char *t, vector<uchar> &p);
 extern void getstring(char *t, ucharbuf &p, int len = MAXTRANS);
 extern void filtertext(char *dst, const char *src, int whitespace = 1, int len = sizeof(string)-1);
-extern void filtername(char *dst, const char *src);
 extern void filterrichtext(char *dst, const char *src, int len = sizeof(string)-1);
 extern void filterservdesc(char *dst, const char *src, int len = sizeof(string)-1);
 extern void filterlang(char *d, const char *s);
 extern void trimtrailingwhitespace(char *s);
 extern void cutcolorstring(char *text, int len);
+static inline void filtername(char *dst, const char *src)
+{
+    filtertext(dst, src, 1, MAXNAMELEN);
+    trimtrailingwhitespace(dst);
+}
 extern void startintermission();
 extern void restoreserverstate(vector<entity> &ents);
 extern string mastername;
