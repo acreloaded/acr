cd9e2043b63d2aba14c232473eb7e20687030b62
Victor <theonlypwner@users.noreply.github.com>
2014-06-28 16:18:47 -0600

    Restore FRAG_ and STREAK_ enumerations

--- source/src/weapon.h
+++ source/src/weapon.h
@@ -21,6 +21,31 @@ enum
 #define ads_gun(g) (!melee_weap(g) && g != GUN_GRENADE && g != GUN_AKIMBO)
 #define ads_classic_allowed(g) (!m_classic(gamemode, mutators) || sniper_weap(g) || g == GUN_HEAL)
 
+enum
+{
+    FRAG_NONE = 0,
+    FRAG_GIB = 1 << 0,
+    FRAG_SCOPE_NONE = 1 << 1,
+    FRAG_SCOPE_FULL = 1 << 2,
+    FRAG_REVENGE = 1 << 3,
+    FRAG_CRIT = 1 << 4,
+    FRAG_FLAG = 1 << 5,
+    FRAG_COMEBACK = 1 << 6,
+    FRAG_FIRST = 1 << 7,
+    FRAG_VALID = (1 << ((7) + 1)) - 1
+}; // up to 1 << 6 is optimal
+
+enum
+{
+    STREAK_AIRSTRIKE = 0,
+    STREAK_RADAR,
+    STREAK_NUKE,
+    STREAK_DROPNADE,
+    STREAK_REVENGE,
+    STREAK_JUG,
+    STREAK_NUM
+};
+
 class playerent;
 class bounceent;
 
