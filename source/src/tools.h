9fbc6b086ab096d3a68ea8bf8d08fc59dc5cee58
Victor <theonlypwner@users.noreply.github.com>
2014-07-09 21:14:16 -0600

    Fix loopvjrev

--- source/src/tools.h
+++ source/src/tools.h
@@ -203,7 +203,7 @@ inline bool findpattern (const char *s, char *d) // returns true if there is mor
 #define loopvj(v)   for(int j = 0; j<(v).length(); j++)
 #define loopvk(v)   for(int k = 0; k<(v).length(); k++)
 #define loopvrev(v) for(int i = (v).length()-1; i>=0; i--)
-#define loopvjrev(v) for(int j = (v).length()-1; j>=0; i--)
+#define loopvjrev(v) for(int j = (v).length()-1; j>=0; j--)
 
 template <class T>
 struct databuf
