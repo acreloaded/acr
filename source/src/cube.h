33471af86ca71a90065a65de3868c64196b0f936
Victor <theonlypwner@users.noreply.github.com>
2014-08-20 17:41:40 -0600

    First beta version for 2.7: release 2.6.1! * Update version number * Regenerate config/docs.cfg

--- source/src/cube.h
+++ source/src/cube.h
@@ -61,7 +61,7 @@ extern int hwtexsize, hwmaxaniso;
 extern int numspawn[3], maploaded, numflagspawn[2];
 extern int verbose;
 
-#define AC_VERSION 10000
+#define AC_VERSION 20601
 #define AC_MASTER_URI "ms.acr.victorz.ca"
 #define AC_MASTER_PORT 80
 #ifndef AC_MASTER_URI
