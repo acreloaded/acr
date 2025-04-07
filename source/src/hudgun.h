3a3edd5c25e4f37f9582480004f120179c865327
Victor <theonlypwner@users.noreply.github.com>
2025-04-06 19:45:05 -0600

    Add HUD gun sway when switching and reloading weapons

--- source/src/hudgun.h
+++ source/src/hudgun.h
@@ -39,15 +39,7 @@ struct weaponmove
 
         swaytest.x = swaytest.y = swaytest.z = 0;
 
-        if(p->weaponchanging)
-        {
-            //anim = ANIM_GUN_RELOAD;
-            anim = ANIM_GUN_IDLE;
-            basetime = p->weaponchanging;
-            float progress = clamp((lastmillis - p->weaponchanging) / (float)SWITCHTIME(p->perk1 == PERK_TIME), 0.0f, 1.0f);
-            k_rot = -90*sinf(progress*M_PI);
-        }
-        else if(p->weaponsel->reloading)
+        if(p->weaponsel->reloading)
         {
             anim = ANIM_GUN_RELOAD;
             basetime = p->weaponsel->reloading;
@@ -69,70 +61,75 @@ struct weaponmove
             anim = ANIM_GUN_IDLE;
             basetime = lastaction;
 
-            int timediff = lastmillis-lastaction,
-                animtime = min(p->weaponsel->gunwait, (int)p->weaponsel->info.attackdelay);
-            vec sway = aimdir;
-            float progress = 0.0f;
-            float k_back = 0.0f;
+            if (p->weaponchanging)
+            {
+                float progress = clamp((lastmillis - p->weaponchanging) / (float)SWITCHTIME(p->perk1 == PERK_TIME), 0.0f, 1.0f);
+                k_rot = -90 * sinf(progress * M_PI);
+            }
 
             if (p->weaponsel == p->lastattackweapon || p->weaponsel->type == GUN_RPG || p->weaponsel->type == GUN_HEAL)
             {
-                progress = max(0.0f, min(1.0f, timediff/(float)animtime));
+                int timediff = lastmillis - lastaction,
+                    animtime = min(p->weaponsel->gunwait, (int)p->weaponsel->info.attackdelay);
+                float progress = max(0.0f, min(1.0f, timediff / (float)animtime));
                 // f(x) = -sin(x-1.5)^3
-                kick = -sinf(pow((1.5f*progress)-1.5f,3));
+                kick = -sinf(pow((1.5f * progress) - 1.5f, 3));
                 kick *= p->eyeheight / p->maxeyeheight;
                 if (p->weaponsel->type == GUN_HEAL)
                     basetime = lastmillis - (int)(p->zoomed * 1000.f);
                 else if (p->lastaction || p->weaponsel->type == GUN_RPG)
                     anim = p->weaponsel->modelanim();
             }
-
-            if(p->weaponsel->info.mdl_kick_rot || p->weaponsel->info.mdl_kick_back)
-            {
-                k_rot = p->weaponsel->info.mdl_kick_rot*kick;
-                k_back = p->weaponsel->info.mdl_kick_back*kick/10;
-            }
-
-            if(nosway) sway.x = sway.y = sway.z = 0;
-            else
-            {
-                float swayspeed = sinf((float)swaymillis/swayspeeddiv)/(swaymovediv/10.0f);
-                float swayupspeed = cosf((float)swaymillis/swayupspeeddiv)/(swayupmovediv/10.0f);
-
-                float plspeed = min(1.0f, sqrtf(p->vel.x*p->vel.x + p->vel.y*p->vel.y));
-
-                swayspeed *= plspeed/2;
-                swayupspeed *= plspeed/2;
-
-                swap(sway.x, sway.y);
-                sway.y = -sway.y;
-
-                swayupspeed = fabs(swayupspeed); // sway a semicirle only
-                sway.z = 1.0f;
-
-                sway.x *= swayspeed;
-                sway.y *= swayspeed;
-                sway.z *= swayupspeed;
-
-                sway.mul(p->eyeheight / p->maxeyeheight);
-
-                if (ads_gun(p->weaponsel->type))
-                {
-                    const float zoom_eased = sqrtf(p->zoomed);
-                    k_rot *= 1 - zoom_eased / 2.f;
-                    k_back *= 1 - zoom_eased / 1.1f;
-                    sway.mul(1 - zoom_eased / 1.2f);
-                    swaydir.mul(1 - zoom_eased / 1.3f);
-                }
-            }
-
-            pos.add(swaydir);
-            pos.x -= aimdir.x*k_back+sway.x;
-            pos.y -= aimdir.y*k_back+sway.y;
-            pos.z -= aimdir.z*k_back+sway.z;
-
-            (swaytest = aimdir).mul(-k_back).add(sway);
         }
+
+        vec sway = aimdir;
+        float k_back = 0.0f;
+
+        if(p->weaponsel->info.mdl_kick_rot || p->weaponsel->info.mdl_kick_back)
+        {
+            k_rot += p->weaponsel->info.mdl_kick_rot*kick;
+            k_back = p->weaponsel->info.mdl_kick_back*kick/10;
+        }
+
+        if(nosway) sway.x = sway.y = sway.z = 0;
+        else
+        {
+            float swayspeed = sinf((float)swaymillis/swayspeeddiv)/(swaymovediv/10.0f);
+            float swayupspeed = cosf((float)swaymillis/swayupspeeddiv)/(swayupmovediv/10.0f);
+
+            float plspeed = min(1.0f, sqrtf(p->vel.x*p->vel.x + p->vel.y*p->vel.y));
+
+            swayspeed *= plspeed/2;
+            swayupspeed *= plspeed/2;
+
+            swap(sway.x, sway.y);
+            sway.y = -sway.y;
+
+            swayupspeed = fabs(swayupspeed); // sway a semicirle only
+            sway.z = 1.0f;
+
+            sway.x *= swayspeed;
+            sway.y *= swayspeed;
+            sway.z *= swayupspeed;
+
+            sway.mul(p->eyeheight / p->maxeyeheight);
+
+            if (ads_gun(p->weaponsel->type))
+            {
+                const float zoom_eased = sqrtf(p->zoomed);
+                k_rot *= 1 - zoom_eased / 2.f;
+                k_back *= 1 - zoom_eased / 1.1f;
+                sway.mul(1 - zoom_eased / 1.2f);
+                swaydir.mul(1 - zoom_eased / 1.3f);
+            }
+        }
+
+        pos.add(swaydir);
+        pos.x -= aimdir.x*k_back+sway.x;
+        pos.y -= aimdir.y*k_back+sway.y;
+        pos.z -= aimdir.z*k_back+sway.z;
+
+        (swaytest = aimdir).mul(-k_back).add(sway);
     }
 };
 
